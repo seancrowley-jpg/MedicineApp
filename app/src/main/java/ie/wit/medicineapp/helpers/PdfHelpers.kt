44d4fd9d6b1184aac015beeea17ee2b2c02763ae
Sean-Crowley <seancrowley033@gmail.com>
2022-03-03 13:06:16 +0000

    Medicine details and lists can now be printed

--- app/src/main/java/ie/wit/medicineapp/helpers/PdfHelpers.kt
+++ app/src/main/java/ie/wit/medicineapp/helpers/PdfHelpers.kt
@@ -70,3 +70,57 @@ fun getFile(fileName: String, context:Context): File {
     val documentsDirectory = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
     return File.createTempFile(fileName, ".pdf", documentsDirectory)
 }
+
+fun createPdf(medicine: MedicineModel, context:Context,userName:String) : File{
+    val doc = Document()
+    val fileName = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(System.currentTimeMillis())
+    val filePath = File.createTempFile(fileName, ".pdf")
+    try{
+        PdfWriter.getInstance(doc, FileOutputStream(filePath))
+        doc.open()
+        doc.addTitle("Medication")
+        val title = Chunk("Medication for $userName")
+        doc.add(Paragraph(title))
+        doc.add(Paragraph(" "))
+        doc.add(Paragraph("Name: " +medicine.name))
+        doc.add(Paragraph("Dosage: " +medicine.dosage))
+        doc.add(Paragraph("Usage Directions: " +medicine.usageDir))
+        doc.add(Paragraph("Remaining Quantity: " +medicine.quantity))
+        doc.close()
+
+    }
+    catch (e: Exception){
+        Toast.makeText(context,e.message, Toast.LENGTH_SHORT).show()
+    }
+    return filePath
+}
+
+fun createListPdf(medicineList: ArrayList<MedicineModel>, context:Context, userName:String): File{
+    val doc = Document()
+    val fileName = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(System.currentTimeMillis())
+    val filePath = File.createTempFile(fileName, ".pdf")
+
+    try{
+        PdfWriter.getInstance(doc, FileOutputStream(filePath))
+        doc.open()
+        doc.addTitle("Medication")
+        val title = Chunk("Medication for $userName")
+        doc.add(Paragraph(title))
+        val lineSeparator = LineSeparator()
+        lineSeparator.lineColor = BaseColor(0, 0, 0, 68)
+        for (medicine in medicineList){
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph("Name: " +medicine.name))
+            doc.add(Paragraph("Dosage: " +medicine.dosage))
+            doc.add(Paragraph("Usage Directions: " +medicine.usageDir))
+            doc.add(Paragraph("Remaining Quantity: " +medicine.quantity))
+            doc.add(Paragraph(" "))
+            doc.add(lineSeparator)
+        }
+        doc.close()
+    }
+    catch (e: Exception){
+        Toast.makeText(context,e.message, Toast.LENGTH_SHORT).show()
+    }
+    return filePath
+}
