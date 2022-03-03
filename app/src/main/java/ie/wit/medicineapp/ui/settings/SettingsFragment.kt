19c9ef18801e47347ff8917c36aede6084a12059
Sean-Crowley <seancrowley033@gmail.com>
2022-03-03 14:23:09 +0000

    Created Settings fragment and viewmodel added Andriod x preference library added new stings and option in nav drawer

---
+++ app/src/main/java/ie/wit/medicineapp/ui/settings/SettingsFragment.kt
@@ -0,0 +1,22 @@
+package ie.wit.medicineapp.ui.settings
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ie.wit.medicineapp.R
+
+class SettingsFragment : Fragment() {
+    
+    private lateinit var viewModel: SettingsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+}
